3628e0535d0b26364d4c9b01a9b557f7e0a6fdac
Valtýr Örn Kjartansson <valtyr@gmail.com>
2017-08-15 17:08:48 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'innuscrape',
+    packages = ['innuscrape'],
+    version = '1.0',
+    description = 'Scraping utility for schedules on inna.is',
+    author = 'Valtýr Örn Kjartansson',
+    author_email = 'valtyr@gmail.com',
+    url = 'https://github.com/valtyrorn/innuscrape',
+    download_url = 'https://github.com/valtyrorn/innuscrape/archive/master.zip',
+    keywords = ['inna', 'scrape'],
+    classifiers = [],
+)
+ 
